2d86baa8bbd60275a163da05a24fc596ea9cb40f
johnny_fan <Johnny_Fan@asus.com>
2021-03-04 15:36:03 +0800

    Update the apt sourcelist to tsinghua mirror source in ubuntu.

---
+++ update_apt_sourcelist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+mv /etc/apt/sources.list /etc/apt/sources.list.bak
+cat << EOF > /etc/apt/sources.list
+# The source image is annotated by default to speed up APT Update. You can uncomment it if necessary
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse
+
+# Pre-release software source, not recommended
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse
+EOF
+apt-get update
